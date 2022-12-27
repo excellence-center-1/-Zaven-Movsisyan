f4b00edb2a8323ea0b49778165af150eadf3014a
zavmovsisyan05 <100776306+zavmovsisyan05@users.noreply.github.com>
2022-12-27 21:07:28 +0400

    Rectangles

--- Python/21-12-22/OOP/Task-5/Task5.py
+++ Python/21-12-22/OOP/Task-5/Task5.py
@@ -1,14 +1,17 @@
 class Rectangle:
-    def __init__(self, x, y, w, h):
-        self.x = x
-        self.y = y
+    def __init__(self, x1, y1, w, h):
+        self.x1 = x1
+        self.y1 = y1
         self.w = w
         self.h = h
+        self.x2 = x1 + w
+        self.y2 = y1 + h
+
     def get_x(self):
-        return self.x
+        return self.x1
     
     def get_y(self):
-        return self.y
+        return self.y1
 
     def get_w(self):
         return self.w
@@ -17,9 +20,26 @@ class Rectangle:
         return self.h
 
     
-    def intersection(self, r):    
-        res = None
-        for i in range(r.get_x(), r.get_x() + 1):
-            print('aaa')
+    def intersection(self, new_rect):
+        n_x1 = new_rect.x1
+        n_x2 = new_rect.x2
+        n_y1 = new_rect.y1
+        n_y2 = new_rect.y2
+        l_x = max(self.x1, n_x1)
+        l_y = max(self.y1, n_y1)
+
+        r_x = min(self.x2, n_x2)
+        r_y = min(self.y2, n_y2)
+        
+        if l_x >= r_x or l_y >= r_y:
+            return None
+        return Rectangle(l_x, l_y, r_x - l_x, r_y - l_y)
             
-            
+        
+rect1 = Rectangle(0, 0, 10, 10)
+rect2 = Rectangle(5, 5, 10, 10)
+rect3 = rect1.intersection(rect2)
+if rect3 is None:
+    print('No intersection')
+else:
+    print(rect3.get_x(), rect3.get_y(), rect3.get_w(), rect3.get_h())

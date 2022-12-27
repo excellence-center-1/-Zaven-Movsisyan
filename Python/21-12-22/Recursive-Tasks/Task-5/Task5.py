6578a2cf41f10b25d717066e909d5a9a6d82b05a
zavmovsisyan05 <100776306+zavmovsisyan05@users.noreply.github.com>
2022-12-27 22:32:50 +0400

    List linearization

--- Python/21-12-22/Recursive-Tasks/Task-5/Task5.py
+++ Python/21-12-22/Recursive-Tasks/Task-5/Task5.py
@@ -1,7 +1,12 @@
 def linear(some_list):
+    ints = [1 if type(i) != list else 0 for i in some_list]
+    if all(ints):
+        print(some_list)
     for i in some_list:
         if type(i) == list:
-            some_list.extend(linear(i))
-    return some_list
+            temp = i
+            some_list.remove(i)
+            some_list.extend(i)
+            linear(some_list)
 
-print(linear([1, 2, [3, 4, [4, 5, 54, [3, 4]]], [1, 2]]))
+linear([[1, 2], 4, [[2, 4, 8, [-4, "er", [0], [{2: "primer"}], []]]]])
